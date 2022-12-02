ce6900953a9314de6e16b77c278a2a9999b984ea
cho407 <parfume407@gmail.com>
2022-12-02 12:15:26 +0900

    fetchData

---
+++ CovidCenter/CovidCenter/WebService.swift
@@ -0,0 +1,20 @@
+//
+//  WebService.swift
+//  CovidCenter
+//
+//  Created by Jongwook Park on 2022/11/22.
+//
+
+import Foundation
+
+class WebService {
+    func fetchData(url: String) async throws -> [CovidCenter] {
+        guard let url = URL(string: url) else { return [] }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let centers = try JSONDecoder().decode(CovidCenters.self, from: data)
+        
+        return centers.data
+    }
+}
+
